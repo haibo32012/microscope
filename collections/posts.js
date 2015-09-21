4d4e3fe30012c2a84814bf9777d4a0547d317f0a
haibo <haibo32012@gmail.com>
2015-09-21 23:22:36 +0800

    collection

---
+++ collections/posts.js
@@ -0,0 +1,70 @@
+Posts = new Mongo.Collection('posts');
+
+Posts.allow({
+	update: function(userId, post) {return ownsDocument(userId, post);},
+	remove: function(userId, post) {return ownsDocument(userId, post);}
+});
+
+Posts.deny({
+	update: function(userId, post, fieldNames) {
+		return (_.without(fieldNames, 'url', 'title').length > 0);
+	}
+});
+
+Meteor.methods({
+	postInsert: function(postAttributes) {
+		check(Meteor.userId(), String);
+		check(postAttributes, {
+			title: String,
+			url: String
+		});
+
+
+		var postWithSameLink = Posts.findOne({url: postAttributes.url});
+		if (postWithSameLink) {
+			return {
+				postExists: true,
+				_id: postWithSameLink._id
+			}
+		}
+
+		var user = Meteor.user();
+		var post = _.extend(postAttributes, {
+			userId: user._id,
+			author: user.username,
+			submitted: new Date(),
+			commentsCount: 0,
+			upvoters: [],
+			votes: 0
+		});
+
+		var postId = Posts.insert(post);
+
+		return {
+			_id: postId
+		};
+	},
+
+	upvote: function(postId) {
+		check(this.userId, String);
+		check(postId, String);
+
+		var post = Posts.findOne(postId);
+		if(!post)
+			throw new Meteor.Error('invalid', 'Post not found');
+
+		if (_.include(post.upvoters, this.userId))
+			throw new Meteor.Error('invalid', 'Already upvoted this post');
+
+		var affected = Posts.update({
+			_id: postId,
+			upvoters: {$ne: this.userId}
+		}, {
+			$addToSet: {upvoters: this.userId},
+			$inc: {votes: 1}
+		});
+
+		if (! affected)
+			throw new Meteor.Error('invalid', "You weren't able to upvote that post");
+	}
+});
